bc26c03c0edb1d84b0e2c4affb9594fe7fd65f63
Hyun <itsmine096@gmail.com>
2022-05-16 20:14:38 +0900

    실습 프로젝트 (doit-my-self)

---
+++ doit-my-self/src/main/java/spring/doitmyself/DoitMySelfApplication.java
@@ -0,0 +1,13 @@
+package spring.doitmyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitMySelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoitMySelfApplication.class, args);
+	}
+
+}
---
+++ doit-my-self/src/test/java/spring/doitmyself/DoitMySelfApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.doitmyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitMySelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
