cf92dff3fa65323d5ab30c98c0882ee50d67984f
Hyun <itsmine096@gmail.com>
2022-08-08 21:59:00 +0900

    #testCode - 프로젝트 생성

---
+++ SpringGuide/testCode/src/test/java/com/springboot/testCode/TestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.testCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
