165754ac887088b1b2778d6397f7e9953b2e9d92
Hyun <itsmine096@gmail.com>
2022-08-15 13:18:04 +0900

    #testCode - Service 설계

---
+++ SpringGuide/testCode/src/main/java/com/springboot/testCode/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.springboot.testCode.service;
+
+import com.springboot.testCode.data.dto.ProductDto;
+import com.springboot.testCode.data.dto.ProductResponseDto;
+import com.springboot.testCode.data.repository.ProductRepository;
+
+public interface ProductService {
+
+    ProductResponseDto getProduct(Long number);
+
+    ProductResponseDto saveProduct(ProductDto productDto);
+
+    ProductRepository changeProductName(Long number, String name) throws Exception;
+
+    void deleteProduct(Long number) throws Exception;
+}
