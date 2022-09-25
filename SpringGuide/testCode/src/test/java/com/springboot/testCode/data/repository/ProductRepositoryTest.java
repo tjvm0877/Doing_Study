123909227f9d6f2850d2835fe9175277acbf2238
Hyun <itsmine096@gmail.com>
2022-09-25 15:39:34 +0900

    데이터 베이스 조회 테스트

---
+++ SpringGuide/testCode/src/test/java/com/springboot/testCode/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.springboot.testCode.data.repository;
+
+import com.springboot.testCode.data.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void save() {
+        Product product = new Product();
+        product.setName("펜");
+        product.setPrice(1000);
+        product.setStock(1000);
+
+        Product savedProduct = productRepository.save(product);
+
+
+        assertEquals(product.getName(), savedProduct.getName());
+        assertEquals(product.getPrice(), savedProduct.getPrice());
+        assertEquals(product.getStock(), savedProduct.getStock());
+    }
+}
