83dd7b03b66c1c0c3c4dfa9871e82876d326a0c1
Hyun <itsmine096@gmail.com>
2022-09-26 17:58:45 +0900

    JPA활용 프로젝트 생성

---
+++ SpringGuide/advenced_jpa/src/main/java/com/spring/advenced_jpa/AdvencedJpaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.advenced_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvencedJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvencedJpaApplication.class, args);
+	}
+
+}
---
+++ SpringGuide/advenced_jpa/src/test/java/com/spring/advenced_jpa/AdvencedJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.advenced_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvencedJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
