c064387e620109cc2f24ff6f30139868509868ff
Hyun <itsmine096@gmail.com>
2022-09-26 18:42:12 +0900

    [docs] Product 엔티티 주석 추가

--- SpringGuide/advenced_jpa/src/main/java/com/spring/advenced_jpa/data/entity/Product.java
+++ SpringGuide/advenced_jpa/src/main/java/com/spring/advenced_jpa/data/entity/Product.java
@@ -16,20 +16,26 @@ import java.time.LocalDateTime;
 @ToString
 public class Product {
 
+    // 상품 번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long number;
 
+    // 상품 이름
     @Column(nullable = false)
     private String name;
 
+    // 상품 가격
     @Column(nullable = false)
     private int price;
 
+    // 상품 재고
     @Column(nullable = false)
     private int stock;
 
+    // 생성 날짜
     private LocalDateTime createdAt;
 
+    // 업데이트 날짜
     private LocalDateTime updatedAt;
 }
