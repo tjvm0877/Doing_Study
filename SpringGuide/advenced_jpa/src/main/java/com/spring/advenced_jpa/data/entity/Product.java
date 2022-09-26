eaf4862640988805ab34c463ebc70c8af7e71f83
Hyun <itsmine096@gmail.com>
2022-09-26 18:33:16 +0900

    [feat] 엔티티 생성

---
+++ SpringGuide/advenced_jpa/src/main/java/com/spring/advenced_jpa/data/entity/Product.java
@@ -0,0 +1,35 @@
+package com.spring.advenced_jpa.data.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "product")
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stock;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
