8cff35b71348d8d4353f99fdd154b2c1f0d83daf
Hyun <itsmine096@gmail.com>
2022-09-26 19:12:06 +0900

    [docs] PrductResponseDto 주석 추가

--- SpringGuide/advenced_jpa/src/main/java/com/spring/advenced_jpa/data/dto/ProductResponseDto.java
+++ SpringGuide/advenced_jpa/src/main/java/com/spring/advenced_jpa/data/dto/ProductResponseDto.java
@@ -10,8 +10,15 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductResponseDto {
+    // 상품 번호
     private Long number;
+
+    // 상품 이름
     private String name;
+
+    // 상품 가격
     private int price;
+
+    // 상품 재고
     private int stock;
 }
