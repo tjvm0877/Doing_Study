b4de6fda11ef9eba9e74e7e30d2209bf26f92119
Hyun <itsmine096@gmail.com>
2022-09-28 16:39:32 +0900

    [feat] mvc, 템플릿 엔진을 이용한 페이지 구현

--- InflearnSpring/spring-project/src/main/java/com/hyun/springproject/controller/HelloController.java
+++ InflearnSpring/spring-project/src/main/java/com/hyun/springproject/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.hyun.springproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,4 +15,10 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
+    }
 }
